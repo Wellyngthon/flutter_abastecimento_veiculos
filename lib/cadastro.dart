6807ec99f7978fab96658256c84a4228e45095d4
Wellyngton <wgl_35@hotmail.com>
2025-11-14 03:21:45 -0400

    feat(auth): autenticação de usuário

---
+++ lib/cadastro.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:abastecimento_veiculos/login.dart';
+
+class CadastroTela extends StatefulWidget {
+  @override
+  _CadastroTelaState createState() => _CadastroTelaState();
+}
+
+class _CadastroTelaState extends State<CadastroTela> {
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _senhaController = TextEditingController();
+
+  bool _isLoading = false;
+
+  Future<void> _btnCadastro() async {
+    if (_formKey.currentState!.validate()) {
+      setState(() => _isLoading = true);
+      try {
+        await FirebaseAuth.instance.createUserWithEmailAndPassword(
+          email: _emailController.text.trim(),
+          password: _senhaController.text.trim(),
+        );
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Cadastro realizado com sucesso!')),
+        );
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => LoginTela()),
+        );
+      } on FirebaseAuthException catch (e) {      
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text("${e.message}")),
+        );
+      } finally {
+        setState(() => _isLoading = false);
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Cadastro - Gas Station App')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              Image.asset('assets/gasStation.png', height: 150),
+              SizedBox(height: 50),
+              TextFormField(
+                controller: _emailController,
+                decoration: InputDecoration(labelText: 'Email'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "Campo de email vazio";
+                  }
+                  if (!value.contains("@")) {
+                    return "O email não é válido";
+                  }
+                  return null;
+                },
+                keyboardType: TextInputType.emailAddress,
+              ),
+              TextFormField(
+                controller: _senhaController,
+                decoration: InputDecoration(labelText: 'Senha'),
+                obscureText: true,
+                validator: (value) {
+                  if (value == null || value.length < 6) {
+                    return "A senha deve ter pelo menos 6 caracteres";
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 20),
+              _isLoading
+                  ? CircularProgressIndicator()
+                  : ElevatedButton(
+                      onPressed: _btnCadastro,
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.lightGreenAccent,
+                      ),
+                      child: Text('Cadastrar'),
+                    ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/login.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:abastecimento_veiculos/cadastro.dart';
+
+class LoginTela extends StatefulWidget {
+  @override
+  _LoginTelaState createState() => _LoginTelaState();
+}
+
+class _LoginTelaState extends State<LoginTela> {
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _senhaController = TextEditingController();
+
+  bool _isLoading = false;
+
+  Future<void> _realizarLogin() async {
+    if (_formKey.currentState!.validate()) {
+      setState(() => _isLoading = true);
+      try {
+        await FirebaseAuth.instance.signInWithEmailAndPassword(
+          email: _emailController.text.trim(),
+          password: _senhaController.text.trim(),
+        );
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Login realizado com sucesso!')),
+        );
+      } on FirebaseAuthException catch (e) {
+          ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text("${e.message}")),
+        );
+      } finally {
+        setState(() => _isLoading = false);
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Gas Station App')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              Image.asset('assets/gasStation.png', height: 150),
+              SizedBox(height: 50),
+              TextFormField(
+                controller: _emailController,
+                decoration: InputDecoration(labelText: 'Email'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "Campo de email vazio";
+                  }
+                  if (!value.contains("@")) {
+                    return "O email não é válido";
+                  }
+                  return null;
+                },
+                keyboardType: TextInputType.emailAddress,
+              ),
+              TextFormField(
+                controller: _senhaController,
+                decoration: InputDecoration(labelText: 'Senha'),
+                obscureText: true,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "A senha não pode ser vazia";
+                  }
+                  if (value.length < 6) {
+                    return "Senha muito curta";
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 20),
+              _isLoading
+                  ? CircularProgressIndicator()
+                  : ElevatedButton(
+                      onPressed: _realizarLogin,
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.lightGreenAccent,
+                      ),
+                      child: Text('Entrar'),
+                    ),
+              TextButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => CadastroTela()),
+                  );
+                },
+                child: Text('Cadastre-se'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
